17ce172a6d71f56558dd14f63fd7b3ad62e6e295
eat-sleep-code/ <info@eat-sleep-code.com>
2020-09-05 21:41:48 -0700

    Update tiny-tv.py

--- tiny-tv.py
+++ tiny-tv.py
@@ -1,5 +1,4 @@
 import argparse
-import keyboard
 import os
 import subprocess
 import shutil
@@ -104,83 +103,78 @@ try:
 	
 	print('\n Tiny TV ' + version )
 	print('\n ----------------------------------------------------------------------\n')
+	print('\n Press [Ctrl]-C to exit. \n')
 		
-	while True:
-		if keyboard.is_pressed('ctrl+c') or keyboard.is_pressed('esc'):
-			# clear()
-			echoOn()
-			break
-		
-		if input.find('.') == -1 and input.find(';') == -1:
-			input = input + '.mp4'
-		video = input
-		
-		
-		# --- YouTube Download -------------------------------------------------
+	if input.find('.') == -1 and input.find(';') == -1:
+		input = input + '.mp4'
+	video = input
+	
+	
+	# --- YouTube Download -------------------------------------------------
 
-		if input.find('youtube.com') != -1:
-			print(' Starting download of video... ')
-			downloadHeight = 720
-			if maximumVideoHeight >= 4320: # Future product
-				downloadHeight = 4320
-			elif maximumVideoHeight >= 2160: # Minimum Raspberry Pi 4
-				downloadHeight = 2160
-			elif maximumVideoHeight >= 1080: # Minimum Raspberry Pi 3B+
-				downloadHeight = 1080
-			youtubeDownloadOptions = { 
-				'outtmpl': videoCategoryFolder + '%(id)s.%(ext)s',
-				'format': 'best[height=' + str(downloadHeight) + ']'
-			}
-			with youtube_dl.YoutubeDL(youtubeDownloadOptions) as youtubeDownload:
-				info = youtubeDownload.extract_info(input)
-				video = info.get('id', None) + '.' + info.get('ext', None)
-				
-			print(' Setting the owner of the file to current user...')
-			shutil.chown(videoCategoryFolder + video, user='pi', group='pi')
-			if saveAs != 'YOUTUBEID':
-				try:
-					os.rename(videoCategoryFolder + video, videoCategoryFolder + saveAs)
-					video = saveAs
-				except Exception as ex:
-					print(str(ex))
-		
-
-		# --- Pillar Box / Letter Box Removal ----------------------------------
-
-		if removeVerticalBars == True:
-			print(' Starting removal of vertical black bars (this will take a while)... ')
-			subprocess.call('ffmpeg -i "' + videoCategoryFolder + video + '" -filter:v "crop=ih/3*4:ih,scale=-2:' + str(maximumVideoHeight) + ',setsar=1" -c:v libx264 -crf 27 -preset veryfast -c:a copy "' + videoCategoryFolder + '~' + video + '"' , shell=True)
-			os.remove(videoCategoryFolder + video)
-			os.rename(videoCategoryFolder + '~' + video, videoCategoryFolder + video)
-			shutil.chown(videoCategoryFolder + video, user='pi', group='pi')
+	if input.find('youtube.com') != -1:
+		print(' Starting download of video... ')
+		downloadHeight = 720
+		if maximumVideoHeight >= 4320: # Future product
+			downloadHeight = 4320
+		elif maximumVideoHeight >= 2160: # Minimum Raspberry Pi 4
+			downloadHeight = 2160
+		elif maximumVideoHeight >= 1080: # Minimum Raspberry Pi 3B+
+			downloadHeight = 1080
+		youtubeDownloadOptions = { 
+			'outtmpl': videoCategoryFolder + '%(id)s.%(ext)s',
+			'format': 'best[height=' + str(downloadHeight) + ']'
+		}
+		with youtube_dl.YoutubeDL(youtubeDownloadOptions) as youtubeDownload:
+			info = youtubeDownload.extract_info(input)
+			video = info.get('id', None) + '.' + info.get('ext', None)
 			
-		elif removeHorizontalBars == True:
-			print(' Starting removal of horizontal black bars (this will take a while)... ')
-			subprocess.call('ffmpeg -i "' + videoCategoryFolder + video + '" -filter:v "crop=iw:iw/16*9,scale=-2:' + str(maximumVideoHeight) + ',setsar=1" -c:v libx264 -crf 27 -preset veryfast -c:a copy "' + videoCategoryFolder + '~' + video + '"', shell=True)
-			os.remove(videoCategoryFolder + video)
-			os.rename(videoCategoryFolder + '~' + video, videoCategoryFolder + video)
-			shutil.chown(videoCategoryFolder + video, user='pi', group='pi')
+		print(' Setting the owner of the file to current user...')
+		shutil.chown(videoCategoryFolder + video, user='pi', group='pi')
+		if saveAs != 'YOUTUBEID':
+			try:
+				os.rename(videoCategoryFolder + video, videoCategoryFolder + saveAs)
+				video = saveAs
+			except Exception as ex:
+				print(str(ex))
+	
 
-		# --- Resize only ------------------------------------------------------
+	# --- Pillar Box / Letter Box Removal ----------------------------------
 
-		if resize == True and removeVerticalBars == False and removeHorizontalBars == False:
-			print(' Starting resize to maximum video height (this will take a while)... ')
-			subprocess.call('ffmpeg -i "' + videoCategoryFolder + video + '" -filter:v "scale=-2:' + str(maximumVideoHeight) + ',setsar=1" -c:v libx264 -crf 27 -preset veryfast -c:a copy "' + videoCategoryFolder + '~' + video + '"' , shell=True)
-			os.remove(videoCategoryFolder + video)
-			os.rename(videoCategoryFolder + '~' + video, videoCategoryFolder + video)
-			shutil.chown(videoCategoryFolder + video, user='pi', group='pi')
+	if removeVerticalBars == True:
+		print(' Starting removal of vertical black bars (this will take a while)... ')
+		subprocess.call('ffmpeg -i "' + videoCategoryFolder + video + '" -filter:v "crop=ih/3*4:ih,scale=-2:' + str(maximumVideoHeight) + ',setsar=1" -c:v libx264 -crf 27 -preset veryfast -c:a copy "' + videoCategoryFolder + '~' + video + '"' , shell=True)
+		os.remove(videoCategoryFolder + video)
+		os.rename(videoCategoryFolder + '~' + video, videoCategoryFolder + video)
+		shutil.chown(videoCategoryFolder + video, user='pi', group='pi')
 		
-		# --- Playback ---------------------------------------------------------
+	elif removeHorizontalBars == True:
+		print(' Starting removal of horizontal black bars (this will take a while)... ')
+		subprocess.call('ffmpeg -i "' + videoCategoryFolder + video + '" -filter:v "crop=iw:iw/16*9,scale=-2:' + str(maximumVideoHeight) + ',setsar=1" -c:v libx264 -crf 27 -preset veryfast -c:a copy "' + videoCategoryFolder + '~' + video + '"', shell=True)
+		os.remove(videoCategoryFolder + video)
+		os.rename(videoCategoryFolder + '~' + video, videoCategoryFolder + video)
+		shutil.chown(videoCategoryFolder + video, user='pi', group='pi')
 
-		playCount = 0
-		while playCount >= 0:
-			playCount += 1
-			print('\n Starting playback ' + str(playCount) + '...')
-			videoFullPath = videoCategoryFolder + str(video)
-			subprocess.call('omxplayer -o alsa --vol ' + str(volume) + ' "' + videoFullPath + '"', shell=True)
-			time.sleep(10)
-			
-		sys.exit(0)
+	# --- Resize only ------------------------------------------------------
+
+	if resize == True and removeVerticalBars == False and removeHorizontalBars == False:
+		print(' Starting resize to maximum video height (this will take a while)... ')
+		subprocess.call('ffmpeg -i "' + videoCategoryFolder + video + '" -filter:v "scale=-2:' + str(maximumVideoHeight) + ',setsar=1" -c:v libx264 -crf 27 -preset veryfast -c:a copy "' + videoCategoryFolder + '~' + video + '"' , shell=True)
+		os.remove(videoCategoryFolder + video)
+		os.rename(videoCategoryFolder + '~' + video, videoCategoryFolder + video)
+		shutil.chown(videoCategoryFolder + video, user='pi', group='pi')
+	
+	# --- Playback ---------------------------------------------------------
+
+	playCount = 0
+	while playCount >= 0:
+		playCount += 1
+		print('\n Starting playback ' + str(playCount) + '...')
+		videoFullPath = videoCategoryFolder + str(video)
+		subprocess.call('omxplayer -o alsa --vol ' + str(volume) + ' "' + videoFullPath + '"', shell=True)
+		time.sleep(10)
+		
+	sys.exit(0)
 
 except KeyboardInterrupt:
 	echoOn()
