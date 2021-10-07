a56e43192ca90857a627aaf509bd81f5893f22a7
eat-sleep-code/ <info@eat-sleep-code.com>
2021-10-07 12:40:37 -0700

    Update mount-usb-drive.sh

--- mount-usb-drive.sh
+++ mount-usb-drive.sh
@@ -1,5 +1,5 @@
 cd ~
-mkdir /media/pi/USBDrive
+sudo mkdir -p /media/pi/USBDrive
 sudo chown -R pi:pi /media/pi/USBDrive
 sudo mount /dev/sda1 /media/pi/USBDrive -t vfat -o rw,uid=1000,gid=1000
 ln -s /media/pi/USBDrive ~/videos
